89ca9f2844180b60dcf5033cfbf14f951d7d3c73
Gabriel Tetzlaf <tetz.gabriel@gmail.com>
2021-05-20 10:37:30 -0300

    Feature: Added functions to calculate the derivative aproximation

--- scripts/index.js
+++ scripts/index.js
@@ -70,5 +70,41 @@ function createTable() {
 }
 
 function calcula() {
+    if(pontos == "2"){
+        if(calculo == "progressiva") {
+            calculaProg2();
+        } else if(calculo == "central") {
+            calculaCent2();
+        } else if(calculo == "regressiva") {
+            calculaRegr2();
+        }
+    } else {
+        if(calculo == "progressiva") {
+            calculaProg3();
+        } else if(calculo == "central") {
+            alert("Selecione um método válido");
+        } else if(calculo == "regressiva") {
+            calculaRegr3();
+        }
+    }
+}
+
+function calculaProg2() {
+    console.log("Calcula")
+}
+
+function calculaCent2() {
+    console.log("Calcula")
+}
+
+function calculaRegr2() {
+    console.log("Calcula")
+}
+
+function calculaProg3() {
+    console.log("Calcula")
+}
+
+function calculaRegr3() {
     console.log("Calcula")
 }
