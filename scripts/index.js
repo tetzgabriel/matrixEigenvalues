01988dff358389d200bf447183aab1f7aa7f687c
Gabriel Tetzlaf <tetz.gabriel@gmail.com>
2021-05-20 23:26:21 -0300

    Fix: Bug of showing 5 inputs

--- scripts/index.js
+++ scripts/index.js
@@ -16,18 +16,24 @@ function createTable() {
     if(pontos == "2"){
         if(calculo == "progressiva") {
             document.getElementById("error").style.display = "none";  
-            
+            document.getElementById("3pontosRegr").style.display = "none";            
+            document.getElementById("3pontosProg").style.display = "none";
+
             document.getElementById("2pontosRegr").style.display = "none";            
             document.getElementById("2pontosCent").style.display = "none";
             document.getElementById("2pontosProg").style.display = "flex";
         } else if(calculo == "central") {
             document.getElementById("error").style.display = "none";  
+            document.getElementById("3pontosRegr").style.display = "none";            
+            document.getElementById("3pontosProg").style.display = "none";
             
             document.getElementById("2pontosRegr").style.display = "none";            
             document.getElementById("2pontosProg").style.display = "none";            
             document.getElementById("2pontosCent").style.display = "flex";
         } else if(calculo == "regressiva") {
-            document.getElementById("error").style.display = "none";  
+            document.getElementById("error").style.display = "none";
+            document.getElementById("3pontosRegr").style.display = "none";            
+            document.getElementById("3pontosProg").style.display = "none";  
             
             document.getElementById("2pontosCent").style.display = "none";            
             document.getElementById("2pontosProg").style.display = "none";  
